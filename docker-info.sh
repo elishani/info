71024a6d5d1fda5855d78d3d3355c7924d6e68ae
Eli Shani <eli.shani@harmonicinc.com>
2021-12-01 13:31:26 +0200

    update

---
+++ docker-info.sh
@@ -0,0 +1,30 @@
+# DOCKER
+
+# download inmage
+docker pull redis
+
+# get images list
+docker images
+
+# run instanse
+docker run redis
+# run with tag
+docker run redis:4.0
+# run in detach mode
+docker run -d redis
+# run with host port 
+docker run -d -p6000:6794 redis
+# ru with name
+docker run -d -p6000:6794 --name radis-old redis
+
+# stop instanse
+docker stop 87087688 (id/name) /bin/bash
+
+# show all conterianers runnig or -a that not running too
+docker ps [-a]
+
+# prepare image from Dockerfile
+docker builde -t my-app:1.0 .
+
+# to see log
+docker logs 87087688 (id/name)
